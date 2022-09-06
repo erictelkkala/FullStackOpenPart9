aaf4b7b49f189e10f4d08ef7b8009533ba9f266b
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-09-06 11:13:02 +0300

    Task 9.1

---
+++ src/bmiCalculator.ts
@@ -0,0 +1,22 @@
+//  Get the values from the cmd line
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
+
+const calculateBMI = (height: number, weight: number): void => {
+    const BMI: number = weight / Math.pow((height / 100), 2);
+    if (BMI < 18.5) {
+        console.log("Underweight");
+    }
+    else if (BMI >= 18.5 && BMI < 25) {
+        console.log("Normal (healthy weight)");
+    }
+    else if (BMI >= 25 && BMI < 30) {
+        console.log ("Overweight");
+    }
+    else if (BMI >= 30) {
+        console.log("Obese");
+    }
+    else throw new Error("Invalid input");
+};
+
+calculateBMI(height, weight);
